fba1152966dd5dcda5d1a52bbcfe4976081a3323
LindsonAlves <70860702+LindsonCardoso@users.noreply.github.com>
2022-02-21 12:49:50 -0300

    camp_cnpj

--- src/components/InputMask/mask.ts
+++ src/components/InputMask/mask.ts
@@ -46,11 +46,12 @@ export function cep(e: React.FormEvent<HTMLInputElement>) {
   export function cnpj(e: React.FormEvent<HTMLInputElement>) {
     e.currentTarget.maxLength = 15;
     let value = e.currentTarget.value;
-    if (!value.match(/^(\d{3}).(\d{3}).(\d{3})-(\d{2})$/)) {
+    if (!value.match(/^(\d{2}).(\d{3}).(\d{3}).(\d{4})-(\d{2})$/)) {
       value = value.replace(/\D/g, "");
+      value = value.replace(/(\d{2})(\d)/, "$1.$2");
       value = value.replace(/(\d{3})(\d)/, "$1.$2");
-      value = value.replace(/(\d{3})(\d)/, "$1.$2");
-      value = value.replace(/(\d{4})(\d{2})$/, "$1/$2");
+      value = value.replace(/(\d{3})(\d)/, "$1/$2");
+      value = value.replace(/(\d{4})(\d{2})$/, "$1-$2");
       e.currentTarget.value = value;
     }
     return e;
