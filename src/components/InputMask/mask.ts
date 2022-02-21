613bd433a3a039f643dc75ecebbbcab38f13526e
LindsonAlves <70860702+LindsonCardoso@users.noreply.github.com>
2022-02-21 13:00:01 -0300

    input

--- src/components/InputMask/mask.ts
+++ src/components/InputMask/mask.ts
@@ -47,11 +47,13 @@ export function cep(e: React.FormEvent<HTMLInputElement>) {
     e.currentTarget.maxLength = 15;
     let value = e.currentTarget.value;
     if (!value.match(/^(\d{2}).(\d{3}).(\d{3}).(\d{4})-(\d{2})$/)) {
-      value = value.replace(/\D/g, "");
-      value = value.replace(/(\d{2})(\d)/, "$1.$2");
-      value = value.replace(/(\d{3})(\d)/, "$1.$2");
-      value = value.replace(/(\d{3})(\d)/, "$1/$2");
-      value = value.replace(/(\d{4})(\d{2})$/, "$1-$2");
+      value = value.replace(/\D+/g, '');
+      value = value.replace(/(\d{2})(\d)/, '$1.$2');
+      value = value.replace(/(\d{3})(\d)/, '$1.$2');
+      value = value.replace(/(\d{3})(\d)/, '$1/$2');
+      value = value.replace(/(\d{4})(\d)/, '$1-$2');
+      value = value.replace(/(-\d{2})\d+?$/, '$1');
+    
       e.currentTarget.value = value;
     }
     return e;
